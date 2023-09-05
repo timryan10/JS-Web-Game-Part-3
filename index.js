84e2a247923f63d20520acd424e6de119c7a55fa
Tim <rtim10@ymail.com>
2023-09-04 22:21:06 -0400

    part 3 completed

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ function newImage(url, left, bottom){
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null) {
+        character.src = 'assets/green-character/static.gif'
+    }
+
+    if(direction === 'west') {
+        character.src = 'assets/green-character/west.gif'
+    }
+
+    if(direction === 'north') {
+        character.src = 'assets/green-character/north.gif'
+    }
+
+    if(direction === 'east') {
+        character.src = 'assets/green-character/east.gif'
+    }
+
+    if(direction === 'south') {
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
 newImage('assets/pine-tree.png', 450, 350)
@@ -49,3 +72,76 @@ function newInventory(){
 }
 
 const inventory = newInventory()
+
+function move(element){
+    element.style.position = 'fixed'
+    
+
+    function moveToCoordinates(left, bottom){
+        element.style.left = left +'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys (left, bottom, callback){
+        let direction = null; 
+        let x = left;
+        let y= bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        function moveCharacter(){
+        if (direction === 'west') {
+            x-=1
+}
+        if(direction === 'north'){
+            y+=1
+}
+        if(direction === 'east'){
+            x+=1
+}
+        if(direction === 'south'){
+            y-=1
+}
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+
+    setInterval(moveCharacter, 1)
+
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+    
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+    
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        callback(direction)
+    })
+
+    document.addEventListener('keyup', function(e){
+        direction = null
+
+        callback(direction)
+    })
+}
+
+    return{
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }
+}
+
+move (character).to(300,700)
+
